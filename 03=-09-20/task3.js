365a12b3b22fbdbc2e2ef68ed774c9890e0cca43
r4riya <riya.a@innomick.com>
2020-09-03 17:45:53 +0530

    tasks

---
+++ 03=-09-20/task3.js
@@ -0,0 +1,264 @@
+const arrayOfObject = [
+    {
+      "_id": "5f50a51156995c450c5086c6",
+      "index": 0,
+      "guid": "ab19b63c-efbc-4810-867b-4455af3c5157",
+      "isActive": false,
+      "balance": "$3,081.27",
+      "picture": "http://placehold.it/32x32",
+      "age": 24,
+      "eyeColor": "blue",
+      "name": "Ferrell Munoz",
+      "gender": "male",
+      "company": "LINGOAGE",
+      "email": "ferrellmunoz@lingoage.com",
+      "phone": "+1 (824) 409-2536",
+      "address": "927 Canda Avenue, Stonybrook, Kentucky, 8946",
+      "about": "Cillum excepteur esse occaecat laborum incididunt ex enim nulla irure cupidatat consectetur reprehenderit. Ea duis nulla veniam sit dolore. Fugiat enim ex qui commodo velit enim elit reprehenderit consequat culpa nisi officia. Incididunt laboris dolore in esse ex aliquip aute nulla voluptate anim aute. Enim aliquip enim sint reprehenderit laboris ea tempor. Occaecat excepteur deserunt ex ipsum eu ex cillum. Dolore officia consequat nostrud officia voluptate in mollit ipsum pariatur.\r\n",
+      "registered": "2019-06-16T11:18:55 -06:-30",
+      "latitude": -22.447119,
+      "longitude": -140.047503,
+      "tags": [
+        "mollit",
+        "culpa",
+        "officia",
+        "esse",
+        "tempor",
+        "in",
+        "ea"
+      ],
+      "friends": [
+      ],
+      "greeting": "Hello, Ferrell Munoz! You have 1 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5f50a5115b89d6f4e94c1e76",
+      "index": 1,
+      "guid": "69800d93-02a3-4233-a230-7b5dc3bb876e",
+      "isActive": true,
+      "balance": "$1,439.61",
+      "picture": "http://placehold.it/32x32",
+      "age": 21,
+      "eyeColor": "green",
+      "name": "Laurie Short",
+      "gender": "female",
+      "company": "RADIANTIX",
+      "email": "laurieshort@radiantix.com",
+      "phone": "+1 (816) 570-3108",
+      "address": "175 Withers Street, Marshall, New Jersey, 9997",
+      "about": "Ipsum cupidatat ea Lorem commodo eu veniam deserunt do. Eu Lorem est nisi reprehenderit ex fugiat esse sint ad exercitation nostrud. Officia ut id laboris laborum eiusmod veniam quis sint quis nostrud aliquip enim eiusmod. Anim velit elit aliqua magna qui consectetur ullamco quis incididunt.\r\n",
+      "registered": "2014-01-30T03:28:12 -06:-30",
+      "latitude": 0.218718,
+      "longitude": 110.519255,
+      "tags": [
+        "dolor",
+        "culpa",
+        "in",
+        "tempor",
+        "eiusmod",
+        "excepteur",
+        "aliquip"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Loraine Stevens"
+        },
+        {
+          "id": 1,
+          "name": "Wilkins Pittman"
+        },
+        {
+          "id": 2,
+          "name": "Leila Potts"
+        }
+      ],
+      "greeting": "Hello, Laurie Short! You have 9 unread messages.",
+      "favoriteFruit": "strawberry"
+    },
+    {
+      "_id": "5f50a51137a05326a9d24a9c",
+      "index": 2,
+      "guid": "af7d8e0a-c401-4f5f-b897-5c8049de5ca8",
+      "isActive": true,
+      "balance": "$1,830.77",
+      "picture": "http://placehold.it/32x32",
+      "age": 28,
+      "eyeColor": "blue",
+      "name": "Cardenas Maldonado",
+      "gender": "male",
+      "company": "PULZE",
+      "email": "cardenasmaldonado@pulze.com",
+      "phone": "+1 (941) 448-2943",
+      "address": "864 Blake Avenue, Trail, Tennessee, 1677",
+      "about": "Quis in adipisicing anim sit ullamco mollit deserunt adipisicing mollit irure quis tempor irure est. Enim in voluptate reprehenderit pariatur est. Incididunt et ad qui ullamco occaecat tempor id labore occaecat ex qui voluptate Lorem et. Ut ullamco esse anim eu excepteur Lorem consectetur do ut. Cupidatat quis incididunt dolore ea officia est ex non sit ex. Sint sint ea dolore labore ex anim aute laboris.\r\n",
+      "registered": "2014-11-11T04:05:11 -06:-30",
+      "latitude": 60.426337,
+      "longitude": 39.382785,
+      "tags": [
+        "sunt",
+        "anim",
+        "ullamco",
+        "nulla",
+        "dolore",
+        "quis",
+        "cupidatat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Battle Barton"
+        },
+        {
+          "id": 1,
+          "name": "Mia Nichols"
+        },
+        {
+          "id": 2,
+          "name": "Gilliam Crane"
+        }
+      ],
+      "greeting": "Hello, Cardenas Maldonado! You have 3 unread messages.",
+      "favoriteFruit": "apple"
+    },
+    {
+      "_id": "5f50a511ce098e04636f5aed",
+      "index": 3,
+      "guid": "941e90a8-f74b-488b-9f8a-78f013b9574e",
+      "isActive": true,
+      "balance": "$3,788.04",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "green",
+      "name": "Berta Bray",
+      "gender": "female",
+      "company": "QUIZKA",
+      "email": "bertabray@quizka.com",
+      "phone": "+1 (962) 494-2699",
+      "address": "229 Baycliff Terrace, Chapin, Idaho, 4072",
+      "about": "Anim duis duis voluptate voluptate ut velit nisi id sunt. Deserunt laborum esse esse duis do exercitation elit tempor ullamco irure aliqua. Nostrud ad ad minim tempor eiusmod velit sunt aute. Nisi ex excepteur cupidatat eiusmod cillum pariatur id in do incididunt non in. Anim veniam quis dolore sunt tempor ut sunt dolor. Ad laborum deserunt sint occaecat. Deserunt pariatur incididunt duis aliquip ex officia voluptate exercitation anim anim occaecat dolor cillum.\r\n",
+      "registered": "2018-10-01T11:25:12 -06:-30",
+      "latitude": 49.27324,
+      "longitude": 153.525594,
+      "tags": [
+        "laboris",
+        "ipsum",
+        "ea",
+        "consequat",
+        "sunt",
+        "nisi",
+        "id"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Sheree Davis"
+        },
+        {
+          "id": 1,
+          "name": "Mckenzie Ferguson"
+        },
+        {
+          "id": 2,
+          "name": "Golden Noel"
+        }
+      ],
+      "greeting": "Hello, Berta Bray! You have 4 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5f50a511875abf3dce14c2bd",
+      "index": 4,
+      "guid": "6024c9c1-18d2-4e8b-897f-fd0136ee11ce",
+      "isActive": true,
+      "balance": "$2,973.63",
+      "picture": "http://placehold.it/32x32",
+      "age": 31,
+      "eyeColor": "brown",
+      "name": "Shelton Mayo",
+      "gender": "male",
+      "company": "COMBOGENE",
+      "email": "sheltonmayo@combogene.com",
+      "phone": "+1 (828) 400-2317",
+      "address": "689 Goodwin Place, Brady, Connecticut, 9061",
+      "about": "Cillum id sint nisi exercitation cupidatat adipisicing elit velit ad sunt mollit nulla. Incididunt quis do enim elit dolore minim ipsum. Anim proident id mollit Lorem velit ex aliqua aliqua. Incididunt consectetur duis fugiat nostrud.\r\n",
+      "registered": "2018-11-28T10:11:03 -06:-30",
+      "latitude": -42.015276,
+      "longitude": 172.805758,
+      "tags": [
+        "consectetur",
+        "enim",
+        "non",
+        "eu",
+        "duis",
+        "laborum",
+        "irure"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Hendricks Barlow"
+        },
+        {
+          "id": 1,
+          "name": "Gladys Nielsen"
+        },
+        {
+          "id": 2,
+          "name": "Romero Olson"
+        }
+      ],
+      "greeting": "Hello, Shelton Mayo! You have 5 unread messages.",
+      "favoriteFruit": "banana"
+    },
+    {
+      "_id": "5f50a511e35b443b354fe5ab",
+      "index": 5,
+      "guid": "72d7e483-e3e3-4bf1-99b1-b786d11b80e0",
+      "isActive": true,
+      "balance": "$1,317.29",
+      "picture": "http://placehold.it/32x32",
+      "age": 40,
+      "eyeColor": "green",
+      "name": "Helena Pickett",
+      "gender": "female",
+      "company": "GEOFARM",
+      "email": "helenapickett@geofarm.com",
+      "phone": "+1 (826) 569-2710",
+      "address": "922 Lawrence Avenue, Jugtown, Nebraska, 6619",
+      "about": "Sunt est Lorem eu dolore enim ea occaecat id ad aliquip reprehenderit amet dolore quis. Officia duis in est enim ex excepteur et amet nisi nulla minim. Labore pariatur voluptate Lorem do ullamco cillum non ullamco consequat non minim qui. Do veniam laboris nulla aliquip magna. Sint officia sunt qui dolor aliquip cillum voluptate incididunt do.\r\n",
+      "registered": "2015-07-12T05:05:39 -06:-30",
+      "latitude": -45.902741,
+      "longitude": -27.302768,
+      "tags": [
+        "incididunt",
+        "Lorem",
+        "cupidatat",
+        "est",
+        "deserunt",
+        "voluptate",
+        "consequat"
+      ],
+      "friends": [
+        {
+          "id": 0,
+          "name": "Bentley Blair"
+        },
+        {
+          "id": 1,
+          "name": "Gloria Atkins"
+        },
+        {
+          "id": 2,
+          "name": "Hebert Simon"
+        }
+      ],
+      "greeting": "Hello, Helena Pickett! You have 7 unread messages.",
+      "favoriteFruit": "banana"
+    }
+  ]
+
+
+    const friends = arrayOfObject.filter(o => o.friends.length > 0).map(q => q.name)
+    console.log(friends);
